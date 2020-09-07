81521d298128d88444f6d6fd2df8bb4aa7027cfa
volkov-alexandr <volkov.alexandr@gmail.com>
2020-09-07 13:27:28 +0300

    Fixed comments

--- src/main/java/com/example/testtask/BrokenArraySearch.java
+++ src/main/java/com/example/testtask/BrokenArraySearch.java
@@ -3,8 +3,8 @@ package com.example.testtask;
 import java.lang.reflect.Array;
 
 public class BrokenArraySearch {
-    public int findElem(int[] arr, int value) {
 
+    public int findElem(int[] arr, int value) {
         return recursiveSearch(arr, value, 0, Array.getLength(arr)-1);
     }
 
@@ -20,21 +20,21 @@ public class BrokenArraySearch {
         if(arr[mid]>arr[left]){
             //For mid point before zero point
             if(key>arr[mid] ||  (key<arr[mid] && key<arr[left])){
-                //Key point at left side
+                //Key point at right side
                 //It's possible to use interpolation search to be faster
                 return recursiveSearch(arr, key, mid+1, right-1);
             } else {
-                //Key point at right side
+                //Key point at left side
                 return recursiveSearch(arr, key, left+1, mid-1);
             }
         } else {
             //Mid point after zero point
             if(key<arr[mid] ||  (key>arr[mid] && key>arr[left])){
-                //Key point at right side
+                //Key point at left side
                 //It's possible to use interpolation search to be faster
                 return recursiveSearch(arr, key, left+1, mid-1);
             } else {
-                //Key point at left side
+                //Key point at right side
                 return recursiveSearch(arr, key, mid+1, right-1);
             }
         }
